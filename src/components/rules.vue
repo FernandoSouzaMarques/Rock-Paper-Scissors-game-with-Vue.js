2fd34ad3ed522068bd1e1d6027584e48eca5ea42
Fernando S. Marques <fernando.souza@novahaus.com.br>
2020-05-25 22:09:48 -0300

    Add modal rules

--- src/components/rules.vue
+++ src/components/rules.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="m-rules">
-    <div class="m-rules_modal">
-      <div class="m-rules_modal-header">
-        <h2 class="m-rules_modal-title">Rules</h2>
-        <button class="m-rules_modal-close">Close</button>
-      </div>
-      <div class="m-rules_modal-content">
-        <img src="@/assets/image-rules.svg" alt="">
+    <div v-if="rules">
+      <div class="m-rules_overlay" @click="rules = !rules"/>
+      <div class="m-rules_modal">
+        <div class="m-rules_modal-header">
+          <h2 class="m-rules_modal-title">Rules</h2>
+          <button class="m-rules_modal-close" @click="rules = !rules"/>
+        </div>
+        <div class="m-rules_modal-content">
+          <img src="@/assets/image-rules.svg" alt="">
+        </div>
       </div>
     </div>
-    <button class="m-rules_button">Rules</button>
+    <button class="m-rules_button" @click="rules = !rules">Rules</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'rules',
+  data() {
+    return {
+      rules: false,
+    };
+  },
 };
 </script>
